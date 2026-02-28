2163f2a51ff6fdd37387585c437e472b21245d6e
kunle001 <adekunle.olanipekun.ko@gmail.com>
2026-02-28 23:39:08 +0100

    video feed rendering

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -79,7 +79,21 @@ export class ChatComponent implements OnInit, OnDestroy {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
-          { urls: 'stun:stun2.l.google.com:19302' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+          {
+            urls: 'turns:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
         ],
       },
     });
